1d9367941792c34c76dfdb5396b2ff174a7528d5
Glen <glen910115@gmail.com>
2024-09-11 13:10:05 +0900

    Update extract_idx.py

--- utils/extract_idx.py
+++ utils/extract_idx.py
@@ -1,11 +1,11 @@
 import json
 
 # JSON 파일 읽기
-with open('../exam/13_Automation_47.json', 'r', encoding='utf-8') as file:
+with open('../exam/1_CLF_AWS CAF_31.json', 'r', encoding='utf-8') as file:
     quizzes = json.load(file)
 
-# idx 값을 리스트로 추출
-idx_list = [quiz['idx'] for quiz in quizzes]
+# idx 값을 정수형 리스트로 추출
+idx_list = [int(quiz['idx']) for quiz in quizzes]
 
 # idx 리스트 콘솔에 출력
 print(len(idx_list))
